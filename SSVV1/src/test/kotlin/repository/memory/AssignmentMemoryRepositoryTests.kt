fcaea1bd427d3f4415c7fb2c4c334531b3fb89c6
Alex Copindean <alexcopindean@yahoo.com>
2022-03-22 13:45:30 +0200

    Add white box tests

--- SSVV1/src/test/kotlin/repository/memory/AssignmentMemoryRepositoryTests.kt
+++ SSVV1/src/test/kotlin/repository/memory/AssignmentMemoryRepositoryTests.kt
@@ -14,7 +14,61 @@ internal class AssignmentMemoryRepositoryTests {
 
     @Test
     fun saveAssignment_emptyId_throwsException() {
-        val assignment = Assignment("", "", 2, 1)
+        val assignment = Assignment("", "some description", 2, 1)
+
+        assertFails {
+            assignmentRepository.save(assignment)
+        }
+    }
+
+    @Test
+    fun saveAssignment_emptyDescription_throwsException() {
+        val assignment = Assignment("someId", "", 2, 1)
+
+        assertFails {
+            assignmentRepository.save(assignment)
+        }
+    }
+
+    @Test
+    fun saveAssignment_deadlineBelowRange_throwsException() {
+        val assignment = Assignment("someId", "some description", 0, 1)
+
+        assertFails {
+            assignmentRepository.save(assignment)
+        }
+    }
+
+    @Test
+    fun saveAssignment_deadlineAboveRange_throwsException() {
+        val assignment = Assignment("someId", "some description", 15, 1)
+
+        assertFails {
+            assignmentRepository.save(assignment)
+        }
+    }
+
+    @Test
+    fun saveAssignment_deadlineGraterThanStartWeek_throwsException() {
+        val assignment = Assignment("someId", "some description", 8, 10)
+
+        assertFails {
+            assignmentRepository.save(assignment)
+        }
+    }
+
+    @Test
+    fun saveAssignment_startWeekBelowRange_throwsException() {
+        val assignment = Assignment("someId", "some description", 8, 0)
+
+        assertFails {
+            assignmentRepository.save(assignment)
+        }
+    }
+
+    @Test
+    fun saveAssignment_startWeekAboveRange_throwsException() {
+        val assignment = Assignment("someId", "some description", 8, 15)
 
         assertFails {
             assignmentRepository.save(assignment)
